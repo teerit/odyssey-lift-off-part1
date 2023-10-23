10a62c6fb7e5abff9be10871c113bc196b64e309
Teerit Ploensin <teeritploensin@gmail.com>
2023-10-24 00:31:11 +0700

    Add QueryResult component

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,8 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloServer,
-  ApolloProvider,
-} from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
 import GlobalStyles from "./styles";
 import Pages from "./pages";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
